7e448a3617b3c36b0264cd677e7eaa1a6f883b4e
Dima Kolesnik <dmitrykolesnik1@gmail.com>
2019-01-01 22:01:35 +0300

    ftd1 user can start workout

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");  
+const assert = require('assert')  
+
+class Workout{
+  start(){
+    return 'Workout started!';
+  }
+}
+
+class User{
+  startWorkout(){
+    const workout = new Workout();
+    return workout.start();
+  }  
+}
+
+//tests
+
+function createNewUser(){
+  return new User();
+}
+
+user = createNewUser();
+
+assert.deepEqual(user.startWorkout(), 'Workout started!');
