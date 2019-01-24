984206c29f8f9659538470d128a83a7c8a4f236e
Dima Kolesnik <dmitrykolesnik1@gmail.com>
2019-01-24 18:39:26 +0300

    routers added

--- public/app.js
+++ public/app.js
@@ -1,6 +1,11 @@
-new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello! This is your fitness the diary!'
+  },
+  methods: {
+    start: () => {
+      app.message = app.message + '!';
+    }
   }
 });
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const User = require('./User');
+
 const app = express();
 const port = 3000;
 
+const user = new User();
+
 app.use(express.static('public'));
+
+app.get('/workout/start', function(req, res) {
+  res.send(user.startWorkout());
+});
+
+app.get('/workout/end', function(req, res) {
+  res.send(user.endWorkout());
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
